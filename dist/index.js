e2ea7e1cad97a1ee18512e24b38c3e72d7ea9643
noHairMan <79689669+noHairMan@users.noreply.github.com>
2026-06-09 16:47:39 +0800

    feat: 修改默认commit信息

--- dist/index.js
+++ dist/index.js
@@ -52865,8 +52865,11 @@ async function commitChanges(lang) {
         "user.email",
         "41898282+github-actions[bot]@users.noreply.github.com"
     );
+    const repo = process.env.GITHUB_REPOSITORY
+        ? `https://github.com/${process.env.GITHUB_REPOSITORY}`
+        : "https://github.com/dephraiim/translate-readme";
     await git.commit(
-        `docs: Added README."${lang}".md translation via https://github.com/dephraiim/translate-readme`
+        `docs: Added README."${lang}".md translation via ${repo}`
     );
     console.log("finished commit");
     await git.push();
